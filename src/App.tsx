7358b967c03e45cc90ee6633df876c6cfffc1077
Carlos Sujanto <carlos.sujanto@ucalgary.ca>
2025-10-17 13:31:55 -0600

    changing fragment

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import { Canvas, useFrame } from "@react-three/fiber";
 
 import fragmentShader from "./shaders/fragment.glsl";
 import vertexShader from "./shaders/vertex.glsl";
+import { OrbitControls } from "@react-three/drei";
 
 function Box(props: JSX.IntrinsicElements["mesh"]) {
   const ref = useRef<THREE.Mesh>(null!);
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
+  // const [hovered, hover] = useState(false);
+  // const [clicked, click] = useState(false);
 
   useFrame((_, delta) => {
     // console.log("state", state);
     // console.log('delta', delta)
     /* @ts-ignore */
-    ref.current.material.uniforms.uTime.value += 1 * delta;
-    ref.current.rotation.x += 1 * delta;
-    ref.current.rotation.y += 0.5 * delta;
+    ref.current.material.uniforms.uTime.value += .15 * delta;
+    // ref.current.rotation.x += 1 * delta;
+    // ref.current.rotation.y += 0.5 * delta;
   });
 
   useEffect(() => {
@@ -28,10 +29,10 @@ function Box(props: JSX.IntrinsicElements["mesh"]) {
     <mesh
       {...props}
       ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={() => click(!clicked)}
-      onPointerOver={() => hover(true)}
-      onPointerOut={() => hover(false)}
+      // scale={clicked ? 1.5 : 1}
+      // onClick={() => click(!clicked)}
+      // onPointerOver={() => hover(true)}
+      // onPointerOut={() => hover(false)}
     >
       <sphereGeometry args={[1]} />
       <shaderMaterial
@@ -47,6 +48,7 @@ export default function App() {
   return (
     <Canvas>
       <ambientLight intensity={Math.PI / 2} />
+      <OrbitControls/>
       <spotLight
         position={[10, 10, 10]}
         angle={0.15}
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,3 +1,13 @@
+uniform float uTime;
+
+varying vec3 vPosition;
+varying vec3 vNormal;
+varying vec2 vUv;
+
 void main() {
-	gl_FragColor = vec4( 1.0, 1.0, 0.0, 1.0 );
+    vec2 uv = vUv;
+
+    uv.y += uTime;
+
+	gl_FragColor = vec4( vec3(step(0.5,fract(uv.y * 10.0))), 1.0 );
 }
